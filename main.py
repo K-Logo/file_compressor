1ba738ef93804cabd7f4ebedf7cc7f6424545bd7
Will Kukkamalla <122653469+wkukka1@users.noreply.github.com>
2023-03-26 14:10:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 This file is the main file for the text compressor
 """
 from tree import Node, HuffmanTree
+from math import ceil
+import codecs
+
 def open_file(file:str)-> dict():
     """
     >>> open_file("test.txt")
@@ -9,7 +12,7 @@ def open_file(file:str)-> dict():
     :return:
     """
     frequency = {}
-    with open(file) as f:
+    with codecs.open(file, encoding="utf-8") as f:
         lines = f.readlines()
         for i in lines:
             for j in i:
@@ -19,13 +22,52 @@ def open_file(file:str)-> dict():
                     frequency[j] += 1
     return frequency
 
-if __name__ == '__main__':
+def get_binary_values(tree, letters)-> dict():
+    values = {}
+    for i in letters:
+        value = tree.find_huffman_value(i, "")
+        values[i] = value
+    return values
+
+def encode(file:str):
+    # Reads the file and returns a dictionary with all of the letters and their frequencies
     dict = open_file("test.txt")
+    letters = []
+    # Creates the tree
     lst = []
     for i in dict:
         lst.append(Node(i, dict[i]))
-    # tree = HuffmanTree()
-    # tree.add(lst)
+        letters.append(i)
+    tree = HuffmanTree(1)
+    tree.add(lst)
+
+    values = get_binary_values(tree, letters)
+
+    bits_so_far = 0
+
+    string = ""
+
+    # Read through the input file
+    with open(file) as f:
+        lines = f.readlines()
+        for line in lines:
+            for character in line:
+                # for every character, get their bitset, and set those numbers to the bits_out
+                # Nuance: the bits_out are cast to be a Byte Array, and then it's written to the output file
+
+                sequence_bits = values[character]
+
+                string += sequence_bits
+
+            with open("output.txt", "wb") as o:
+                o.write(int(string[::-1], 2).to_bytes(ceil(len(string) / 8), "little"))
+
+
+
+if __name__ == '__main__':
+    # Reads the file and returns a dictionary with all of the letters and their frequencies
+    encode("test.txt")
+
 
 
 
