d54cbbddc8847a75b710d54131ca869c01e026c8
wongbr55 <128927886+wongbr55@users.noreply.github.com>
2023-03-26 14:21:32 -0400

    Add files via upload

---
+++ frontend.py
@@ -0,0 +1,156 @@
+from __future__ import annotations
+from typing import Any
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+
+root = Tk()
+root.geometry('1400x900')
+root.title('File Compressor')
+
+
+# creating new tabs
+tab_control = ttk.Notebook(root)
+tab1 = Frame(tab_control)
+tab2 = Frame(tab_control)
+tab_control.add(tab1, text='Encode or Decode')
+tab_control.add(tab2, text='Tree Visualization')
+tab_control.pack(expand=1, fill='both')
+
+#################################################
+# tab1/encode and decode
+#################################################
+
+new_label = Label(tab1, text = 'Please select an option')
+new_label.place(x=20, y=20, anchor = 'w')
+
+input_text = Text(tab1)
+input_text.place(x=20, y=40, width=750, height=350)
+
+view_label = Label(tab1, text = 'Please select an option')
+view_label.place(x = 20, y = 410, anchor = 'w')
+
+view_box = Text(tab1)
+view_box.place(x=20, y=430, width=750, height=350)
+view_box.config(state=DISABLED)
+
+
+def browse_file() -> None:
+    filename = filedialog.askopenfilename()
+    return filename
+
+
+def encode_ui() -> None:
+    """
+    Displays the GUI for encoding. Called when encode toggle button
+    is clicked.
+    """
+    message = 'Enter the original text here...'
+    new_label.config(text = message)
+    view_label.config(text = 'Encoded text (view only)')
+
+    # these buttons need their commands changed
+    runner_button.config(text = 'Encode!', command = run_decode)
+    huffman_tree_button.config(text = 'Download Huffman Tree', command = download)
+    download_file_button.config(text = 'Download Encoded File and Tree')
+
+    return None
+    
+
+def decode_ui() -> None:
+    """
+    Displays the GUI for decoding. Called when decode toggle button
+    is clicked.
+    """
+    message = 'Enter the encoded text here...'
+    new_label.config(text = message)
+    view_label.config(text = 'Decoded text (view only)')
+
+    # these buttons need their command changed
+    runner_button.config(text = 'Decode!', command = run_encode)
+    huffman_tree_button.config(text = 'Upload Huffman Tree', command = browse_file)
+    download_file_button.config(text = 'Download Decoded File')
+    return None
+
+
+def run_encode() -> None:
+    """
+    Runs the backend
+    Should be connected to a back end funtion
+    """
+    
+    ...
+
+def run_decode() -> None:
+    """
+    Runs the backend
+    Should be connected to a back end funtion
+    """
+    ...
+
+def download() -> None:
+    """
+    This method allows you to download certain files that are outputted
+    This should call the backend methods and return something
+    The return type of None should be changed later
+    """
+    ...
+
+
+# Toggle buttons
+var = IntVar(None, 1)  # '1' represents the default toggle option
+encode_button = Radiobutton(tab1, text = 'Encode', command = encode_ui, variable=var, value = 1)
+encode_button.place(x = 1000, y = 20)
+decode_button = Radiobutton(tab1, text = 'Decode', command = decode_ui, variable=var, value = 2)
+decode_button.place(x = 1100, y = 20)
+
+runner_button = Button(tab1, text = '', width = 30, height=5, command = run_encode)
+runner_button.place(x = 1250, y =150, anchor = 'e')
+
+upload_file_button = Button(tab1, text = 'Upload Text File...', command = browse_file, width = 30, height=5)
+upload_file_button.place(x = 1250, y = 350, anchor = 'e')
+
+huffman_tree_button = Button(tab1, text = '', command = browse_file, width = 30, height=5)
+huffman_tree_button.place(x = 1250, y = 450, anchor = 'e')
+
+download_file_button = Button(tab1, text = '', width = 30, height=5, command = download)
+download_file_button.place(x=1250, y = 550, anchor = 'e')
+encode_ui()
+
+
+
+
+
+
+
+# def helloCallBack():
+#    myLabel = Label(tab2, text='hi')
+# #    myLabel.place(x=500, y=100)
+#    myLabel.pack()
+
+# button = Button(tab1, text='help me', command=helloCallBack)
+# button.place(relx = 0.6, rely = 0.6, anchor = 'center')
+
+# myLabel = Label(tab2, text='hi')
+# myLabel.place(relx = 0.5, rely = 0.5, anchor = 'center')
+
+# Label_middle = Label(tab2,
+#                         text ='Middle')
+# Label_middle.place(relx = 0.4,
+#                    rely = 0.4,
+#                    anchor = 'center')
+
+# # Textbox
+# inputText = Text(tab1, height = 5, width = 20)
+# inputText.pack()
+
+# # Creating a label widget
+# myLabel = Label(root, text='DAVID IS COOL')
+
+# # Shoving it onto the screen
+# # makes it as big as the labels that are being showed
+# myLabel.pack()
+
+root.mainloop()
+
+
---
+++ tree_visualization.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+from typing import Any
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+import igraph as ig
+from igraph import Graph, EdgeSeq
+import plotly.graph_objects as go
+
+
+nr_vertices = 25
+v_label = list(map(str, range(nr_vertices)))
+G = Graph.Tree(nr_vertices, 2) # 2 stands for children number
+lay = G.layout('rt')
+
+position = {k: lay[k] for k in range(nr_vertices)}
+Y = [lay[k][1] for k in range(nr_vertices)]
+M = max(Y)
+
+es = EdgeSeq(G) # sequence of edges
+E = [e.tuple for e in G.es] # list of edges
+
+L = len(position)
+Xn = [position[k][0] for k in range(L)]
+Yn = [2*M-position[k][1] for k in range(L)]
+Xe = []
+Ye = []
+for edge in E:
+    Xe+=[position[edge[0]][0],position[edge[1]][0], None]
+    Ye+=[2*M-position[edge[0]][1],2*M-position[edge[1]][1], None]
+
+labels = v_label
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=Xe,
+                   y=Ye,
+                   mode='lines',
+                   line=dict(color='rgb(210,210,210)', width=1),
+                   hoverinfo='none'
+                   ))
+fig.add_trace(go.Scatter(x=Xn,
+                  y=Yn,
+                  mode='markers',
+                  name='bla',
+                  marker=dict(symbol='circle-dot',
+                                size=18,
+                                color='#6175c1',    #'#DB4551',
+                                line=dict(color='rgb(50,50,50)', width=1)
+                                ),
+                  text=labels,
+                  hoverinfo='text',
+                  opacity=0.8
+                  ))
+
+def tree_visualization(frequency: dict[str, tuple[str]]) -> None:
+    """
+    This is the function that allows us to visualize a tree given the dict of
+    the frequency of the characters
+    """
+    tree = Graph.Tree(len(frequency), 2)
+    lay = tree.layot('rt')
+
