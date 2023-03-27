585e321f0d4d9fd4a1be1b31f8f2f741eb9e840f
Will Kukkamalla <122653469+wkukka1@users.noreply.github.com>
2023-03-27 16:14:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ def get_binary_values(tree, letters)-> dict():
         values[i] = value
     return values
 
-def encode(file:str):
+def encode(file: str):
     # Reads the file and returns a dictionary with all of the letters and their frequencies
-    dict = open_file("test.txt")
+    dict = open_file(file)
     letters = []
     # Creates the tree
     lst = []
@@ -49,7 +49,7 @@ def encode(file:str):
     string = ""
 
     # Read through the input file
-    with open(file) as f:
+    with codecs.open(file, encoding="utf-8") as f:
         lines = f.readlines()
         for line in lines:
             for character in line:
@@ -66,7 +66,7 @@ def encode(file:str):
     return lst
 
 def decode(file: str, key: list):
-    #Creates the tree to decode the text
+    # Creates the tree to decode the text
     tree = HuffmanTree(1)
     tree.add(key)
 
@@ -79,7 +79,7 @@ def decode(file: str, key: list):
         # Iterates the throuh the file to get the bytes
         line_so_far = ""
         string_so_far = ""
-        with open("outputtext.txt", "w") as o:
+        with open("debate_output.txt", "w") as o:
             for line in lines:
 
                 # Turns the byte into a string of either 0 or 1
@@ -98,8 +98,55 @@ def decode(file: str, key: list):
                         string_so_far = ""
                         o.write(char)
 
+def format_file(file:str) -> None:
+    """
+    >>> format_file("warandpeace.txt")
+
+    """
+
+    with open(file, "r", encoding="utf-8") as fic:
+        content = fic.read()
+
+        # lines = fic.readlines()
+        # with open("formated.txt", "w") as f:
+        #     for line in lines:
+        #         for character in line:
+        #             character = str(character)
+        #             f.write(character)
+
+
+    content = content.replace('\n', '')
+    list_of_punctuation = [',', '.', '\"', "\'", '(', ')', '[', "!", '#', '$', '%', '&', '()', '*', '+', '-', '/', ':',
+                           ';', '<', '=', '>', '?', '@', '\\', '^', '_', '`', '{', '|','}', '~', ':', ';']
+
+    for p in list_of_punctuation:
+        content = content.replace(p, "")
+    sentences = content.split(".")
+    sentences = list(map(str.strip, sentences))
+
+    sentences = [s.strip() for s in sentences]
+    with open("formated.txt", "w") as f:
+        for sentence in sentences:
+            f.write(sentence)
+
+
 
 if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod(verbose=True)
+
+    # When you are ready to check your work with python_ta, uncomment the following lines.
+    # (In PyCharm, select the lines below and press Ctrl/Cmd + / to toggle comments.)
+    # You can use "Run file in Python Console" to run PythonTA,
+    # and then also test your methods manually in the console.
+    # import python_ta
+    #
+    # python_ta.check_all(config={
+    #     'max-line-length': 120
+    # })
+
     # Reads the file and returns a dictionary with all of the letters and their frequencies
-    key = encode("test.txt")
+    #format_file("debate.txt")
+    key = encode("debate.txt")
     decode("output.bnr", key)
