2839b5e49ed38a9128521e483a263298b1a1a70d
alexlee27 <105831093+alexlee27@users.noreply.github.com>
2023-04-01 12:48:02 -0400

    implemented dragging image and scrollbar for tree graph

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ from math import ceil
 import codecs
 import struct
 from typing import Any
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 import pandas as pd
 import shutil
 #import tree_visualization
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF, renderPM
+from PIL import Image, ImageTk
 
 
 def browse_tree() -> None:
@@ -36,7 +40,6 @@ def browse_decode() -> str:
 def input_key(key: str) -> list:
     """
     >>> input_key("a5c3")
-
     """
     # do something so it gets the string from the input box
     key_so_far = []
@@ -379,11 +382,236 @@ def decode(file: str, key: str) -> str:
                         o.write(char)
         return line_so_far
 
+def get_svg(svg_file: str) -> None:
+    """Fetch the svg file, and save a file called treegraph.png
+
+    Preconditions:
+        - svg_file is a valid file name of a csv file in the same directory
+    """
+    image = svg2rlg(svg_file)
+    renderPM.drawToFile(image, "treegraph.png", fmt="PNG", dpi=200)
+
+# class AutoScrollbar(ttk.Scrollbar):
+#     ''' A scrollbar that hides itself if it's not needed.
+#         Works only if you use the grid geometry manager '''
+#     def set(self, lo, hi):
+#         if float(lo) <= 0.0 and float(hi) >= 1.0:
+#             self.grid_remove()
+#         else:
+#             self.grid()
+#             ttk.Scrollbar.set(self, lo, hi)
+#
+#     def pack(self, **kw):
+#         raise tk.TclError('Cannot use pack with this widget')
+#
+#     def place(self, **kw):
+#         raise tk.TclError('Cannot use place with this widget')
+#
+# class Zoom_Advanced(ttk.Frame):
+#     ''' Advanced zoom of the image '''
+#     def __init__(self, mainframe, path):
+#         ''' Initialize the main Frame '''
+#         ttk.Frame.__init__(self, master=mainframe)
+#         # Vertical and horizontal scrollbars for canvas
+#         vbar = AutoScrollbar(self.master, orient='vertical')
+#         hbar = AutoScrollbar(self.master, orient='horizontal')
+#         vbar.grid(row=0, column=1, sticky='ns')
+#         hbar.grid(row=1, column=0, sticky='we')
+#         # Create canvas and put image on it
+#         self.canvas = tk.Canvas(self.master, highlightthickness=0,
+#                                 xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+#         self.canvas.grid(row=0, column=0, sticky='nswe')
+#         self.canvas.update()  # wait till canvas is created
+#         vbar.configure(command=self.scroll_y)  # bind scrollbars to the canvas
+#         hbar.configure(command=self.scroll_x)
+#         # Make the canvas expandable
+#         self.master.rowconfigure(0, weight=1)
+#         self.master.columnconfigure(0, weight=1)
+#         # Bind events to the Canvas
+#         self.canvas.bind('<Configure>', self.show_image)  # canvas is resized
+#         self.canvas.bind('<ButtonPress-1>', self.move_from)
+#         self.canvas.bind('<B1-Motion>',     self.move_to)
+#         self.canvas.bind('<MouseWheel>', self.wheel)  # with Windows and MacOS, but not Linux
+#         self.canvas.bind('<Button-5>',   self.wheel)  # only with Linux, wheel scroll down
+#         self.canvas.bind('<Button-4>',   self.wheel)  # only with Linux, wheel scroll up
+#         self.image = Image.open(path)  # open image
+#         self.width, self.height = self.image.size
+#         self.imscale = 1.0  # scale for the canvaas image
+#         self.delta = 1.3  # zoom magnitude
+#         self.container = self.canvas.create_rectangle(0, 0, self.width, self.height, width=0)
+#         self.show_image()
+#
+#     def scroll_y(self, *args, **kwargs):
+#         ''' Scroll canvas vertically and redraw the image '''
+#         self.canvas.yview(*args, **kwargs)  # scroll vertically
+#         self.show_image()  # redraw the image
+#
+#     def scroll_x(self, *args, **kwargs):
+#         ''' Scroll canvas horizontally and redraw the image '''
+#         self.canvas.xview(*args, **kwargs)  # scroll horizontally
+#         self.show_image()  # redraw the image
+#
+#     def move_from(self, event):
+#         ''' Remember previous coordinates for scrolling with the mouse '''
+#         self.canvas.scan_mark(event.x, event.y)
+#
+#     def move_to(self, event):
+#         ''' Drag (move) canvas to the new position '''
+#         self.canvas.scan_dragto(event.x, event.y, gain=1)
+#         self.show_image()  # redraw the image
+#
+#     def wheel(self, event):
+#         ''' Zoom with mouse wheel '''
+#         x = self.canvas.canvasx(event.x)
+#         y = self.canvas.canvasy(event.y)
+#         bbox = self.canvas.bbox(self.container)  # get image area
+#         if bbox[0] < x < bbox[2] and bbox[1] < y < bbox[3]: pass  # Ok! Inside the image
+#         else: return  # zoom only inside image area
+#         scale = 1.0
+#         # Respond to Linux (event.num) or Windows (event.delta) wheel event
+#         if event.num == 5 or event.delta == -120:  # scroll down
+#             i = min(self.width, self.height)
+#             if int(i * self.imscale) < 30: return  # image is less than 30 pixels
+#             self.imscale /= self.delta
+#             scale        /= self.delta
+#         if event.num == 4 or event.delta == 120:  # scroll up
+#             i = min(self.canvas.winfo_width(), self.canvas.winfo_height())
+#             if i < self.imscale: return  # 1 pixel is bigger than the visible area
+#             self.imscale *= self.delta
+#             scale        *= self.delta
+#         self.canvas.scale('all', x, y, scale, scale)  # rescale all canvas objects
+#         self.show_image()
+#
+#     def show_image(self, event=None):
+#         ''' Show image on the Canvas '''
+#         bbox1 = self.canvas.bbox(self.container)  # get image area
+#         # Remove 1 pixel shift at the sides of the bbox1
+#         bbox1 = (bbox1[0] + 1, bbox1[1] + 1, bbox1[2] - 1, bbox1[3] - 1)
+#         bbox2 = (self.canvas.canvasx(0),  # get visible area of the canvas
+#                  self.canvas.canvasy(0),
+#                  self.canvas.canvasx(self.canvas.winfo_width()),
+#                  self.canvas.canvasy(self.canvas.winfo_height()))
+#         bbox = [min(bbox1[0], bbox2[0]), min(bbox1[1], bbox2[1]),  # get scroll region box
+#                 max(bbox1[2], bbox2[2]), max(bbox1[3], bbox2[3])]
+#         if bbox[0] == bbox2[0] and bbox[2] == bbox2[2]:  # whole image in the visible area
+#             bbox[0] = bbox1[0]
+#             bbox[2] = bbox1[2]
+#         if bbox[1] == bbox2[1] and bbox[3] == bbox2[3]:  # whole image in the visible area
+#             bbox[1] = bbox1[1]
+#             bbox[3] = bbox1[3]
+#         self.canvas.configure(scrollregion=bbox)  # set scroll region
+#         x1 = max(bbox2[0] - bbox1[0], 0)  # get coordinates (x1,y1,x2,y2) of the image tile
+#         y1 = max(bbox2[1] - bbox1[1], 0)
+#         x2 = min(bbox2[2], bbox1[2]) - bbox1[0]
+#         y2 = min(bbox2[3], bbox1[3]) - bbox1[1]
+#         if int(x2 - x1) > 0 and int(y2 - y1) > 0:  # show image if it in the visible area
+#             x = min(int(x2 / self.imscale), self.width)   # sometimes it is larger on 1 pixel...
+#             y = min(int(y2 / self.imscale), self.height)  # ...and sometimes not
+#             image = self.image.crop((int(x1 / self.imscale), int(y1 / self.imscale), x, y))
+#             imagetk = ImageTk.PhotoImage(image.resize((int(x2 - x1), int(y2 - y1))))
+#             imageid = self.canvas.create_image(max(bbox2[0], bbox1[0]), max(bbox2[1], bbox1[1]),
+#                                                anchor='nw', image=imagetk)
+#             self.canvas.lower(imageid)  # set image into background
+#             self.canvas.imagetk = imagetk  # keep an extra reference to prevent garbage-collection
+
+class AutoScrollbar(ttk.Scrollbar):
+    ''' A scrollbar that hides itself if it's not needed.
+        Works only if you use the grid geometry manager '''
+    def set(self, lo, hi):
+        if float(lo) <= 0.0 and float(hi) >= 1.0:
+            self.grid_remove()
+        else:
+            self.grid()
+        ttk.Scrollbar.set(self, lo, hi)
+
+    def pack(self, **kw):
+        raise tk.TclError('Cannot use pack with this widget')
+
+    def place(self, **kw):
+        raise tk.TclError('Cannot use place with this widget')
+
+
+class Zoom(ttk.Frame):
+    ''' Simple zoom with mouse wheel '''
+    def __init__(self, mainframe, path):
+        ''' Initialize the main Frame '''
+        ttk.Frame.__init__(self, master=mainframe)
+        # Vertical and horizontal scrollbars for canvas
+        vbar = AutoScrollbar(self.master, orient='vertical')
+        hbar = AutoScrollbar(self.master, orient='horizontal')
+        vbar.grid(row=0, column=1, sticky='ns')
+        hbar.grid(row=1, column=0, sticky='we')
+        # Open image
+        self.image = Image.open(path)
+        # Create canvas and put image on it
+        self.canvas = tk.Canvas(self.master, highlightthickness=0,
+                                xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+        self.canvas.grid(row=0, column=0, sticky='nswe')
+        vbar.configure(command=self.canvas.yview)  # bind scrollbars to the canvas
+        hbar.configure(command=self.canvas.xview)
+        # Make the canvas expandable
+        self.master.rowconfigure(0, weight=1)
+        self.master.columnconfigure(0, weight=1)
+        # Bind events to the Canvas
+        self.canvas.bind('<ButtonPress-1>', self.move_from)
+        self.canvas.bind('<B1-Motion>',     self.move_to)
+        self.canvas.bind('<MouseWheel>', self.wheel)  # with Windows and MacOS, but not Linux
+        self.canvas.bind('<Button-5>',   self.wheel)  # only with Linux, wheel scroll down
+        self.canvas.bind('<Button-4>',   self.wheel)  # only with Linux, wheel scroll up
+        # Show image and plot some random test rectangles on the canvas
+        self.imscale = 1.0
+        self.imageid = None
+        self.delta = 0.75
+        # Text is used to set proper coordinates to the image. You can make it invisible.
+        self.text = self.canvas.create_text(0, 0, anchor='nw', text='Scroll to zoom')
+        self.show_image()
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
+
+    def move_from(self, event):
+        ''' Remember previous coordinates for scrolling with the mouse '''
+        self.canvas.scan_mark(event.x, event.y)
+
+    def move_to(self, event):
+        ''' Drag (move) canvas to the new position '''
+        self.canvas.scan_dragto(event.x, event.y, gain=1)
+
+    def wheel(self, event):
+        ''' Zoom with mouse wheel '''
+        scale = 1.0
+        # Respond to Linux (event.num) or Windows (event.delta) wheel event
+        if event.num == 5 or event.delta == -120:
+            scale        *= self.delta
+            self.imscale *= self.delta
+        if event.num == 4 or event.delta == 120:
+            scale        /= self.delta
+            self.imscale /= self.delta
+        # Rescale all canvas objects
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        self.canvas.scale('all', x, y, scale, scale)
+        self.show_image()
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
+
+    def show_image(self):
+        ''' Show image on the Canvas '''
+        if self.imageid:
+            self.canvas.delete(self.imageid)
+            self.imageid = None
+            self.canvas.imagetk = None  # delete previous image from the canvas
+        width, height = self.image.size
+        new_size = int(self.imscale * width), int(self.imscale * height)
+        imagetk = ImageTk.PhotoImage(self.image.resize(new_size))
+        # Use self.text object to set proper coordinates
+        self.imageid = self.canvas.create_image(self.canvas.coords(self.text),
+                                                anchor='nw', image=imagetk)
+        self.canvas.lower(self.imageid)  # set it into background
+        self.canvas.imagetk = imagetk  # keep an extra reference to prevent garbage-collection
 
 if __name__ == '__main__':
     import doctest
 
     doctest.testmod(verbose=True)
+    get_svg("treegraph.svg")  # Convert svg file to png
 
     root = Tk()
     root.geometry('1400x900')
@@ -409,6 +637,18 @@ if __name__ == '__main__':
     view_box = Entry(tab1)
     view_box.place(x=20, y=430, width=750, height=350)
     # view_box.config(state=DISABLED)
+
+    # Loading the tree graph image (tab2)
+
+    zoomy = Zoom(tab2, 'treegraph.png')
+    # img = Image.open('treegraph.png')
+    #
+    # photo_image = ImageTk.PhotoImage(img)
+    #
+    # tree_graph = Label(tab2, image=photo_image)
+    # tree_graph.pack()
+
+
 
     # Toggle buttons
     var = IntVar(None, 1)  # '1' represents the default toggle option
--- tree_visualization.py
+++ tree_visualization.py
@@ -4,10 +4,11 @@ from tree import HuffmanTree, Node
 
 
 G = pydot.Dot(graph_type="digraph")
+G.size = "7.75,10.25"
 
 # Example tree
 tree = HuffmanTree(1)
-tree.add([Node("c", 1), Node("a", 2), Node("b", 8), Node("d", 6), Node("g", 5), ])
+tree.add([Node("c", 1), Node("a", 2), Node("b", 8), Node("d", 6), Node("g", 5), Node('z', 500), Node('k', 32), Node('q', 100), Node('.', 200), Node('j', 32)])
 
 identifier = 0
 visited_names = set()
@@ -42,42 +43,46 @@ def add_nodes_and_edges(G: pydot.Dot, tree: HuffmanTree, visited_names: set, ide
     elif isinstance(tree._root, Node):
         node = pydot.Node(name=root_name, label=str(tree._root.frequency))
         G.add_node(node)
-    if tree._right is not None:
-        if isinstance(tree._right, HuffmanTree):
-            # Recurse into the right subtree
-            add_nodes_and_edges(G, tree._right, visited_names, new_identifier)
-            edge = pydot.Edge(root_name, str(new_identifier))
-            G.add_edge(edge)
-        elif isinstance(tree._right, Node):
-            # Note that we cannot recurse into a Node object
-            node = pydot.Node(name=str(new_identifier), label=str(tree._right.frequency)+ ' ' + tree._right.character)
-            G.add_node(node)
-            visited_names.add(new_identifier)
-            edge = pydot.Edge(root_name, str(new_identifier))
-            G.add_edge(edge)
-
-    # Refresh new_identifier
-    while new_identifier in visited_names:
-        new_identifier += 1
 
     if tree._left is not None:
         if isinstance(tree._left, HuffmanTree):
             # Recurse into the left subtree
             add_nodes_and_edges(G, tree._left, visited_names, new_identifier)
-            edge = pydot.Edge(root_name, str(new_identifier))
+            edge = pydot.Edge(root_name, str(new_identifier), label='0')
             G.add_edge(edge)
         elif isinstance(tree._left, Node):
             # Note that we cannot recurse into a Node object
             node = pydot.Node(name=str(new_identifier), label=str(tree._left.frequency)+ ' ' + tree._left.character)
             G.add_node(node)
             visited_names.add(new_identifier)
-            edge = pydot.Edge(root_name, str(new_identifier))
+            edge = pydot.Edge(root_name, str(new_identifier), label='0')
             G.add_edge(edge)
+
+    # Refresh new_identifier
+    while new_identifier in visited_names:
+        new_identifier += 1
+
+    if tree._right is not None:
+        if isinstance(tree._right, HuffmanTree):
+            # Recurse into the right subtree
+            add_nodes_and_edges(G, tree._right, visited_names, new_identifier)
+            edge = pydot.Edge(root_name, str(new_identifier), label='1')
+            G.add_edge(edge)
+        elif isinstance(tree._right, Node):
+            # Note that we cannot recurse into a Node object
+            node = pydot.Node(name=str(new_identifier), label=str(tree._right.frequency)+ ' ' + tree._right.character)
+            G.add_node(node)
+            visited_names.add(new_identifier)
+            edge = pydot.Edge(root_name, str(new_identifier), label='1')
+            G.add_edge(edge)
+
+
+
 
 add_nodes_and_edges(G, tree, set(), 0)
 
-im = Image(G.create_jpeg())
+im = Image(G.create_svg())
 
 display(im)
 
-G.write_jpeg("pleaseworkfortheloveofgod.jpeg")
+G.write_svg("treegraph.svg")
